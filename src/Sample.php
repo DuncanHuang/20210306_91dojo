6f28b82b43263f592177d4014cc01a2e0e50b749
Duncan <th710703@gmail.com>
2021-03-06 14:31:51 +0800

    mouson

--- src/Sample.php
+++ src/Sample.php
@@ -12,12 +12,17 @@ class Sample
     public function parser(string $string)
     {
         $players = explode('  ', $string);
+        $output = [];
         foreach ($players as $play){
             $name = explode(':', $play)[0];
             $dices = explode(':', $play)[1];
             $diceSet = explode(' ', $dices);
 
+            $output[$name] = $diceSet;
         }
-        return 'Lin wins, all the same kind:5';
+
+        $winnerName = 'Lin';
+
+        return '' . $winnerName . ' wins, all the same kind:5';
     }
 }
